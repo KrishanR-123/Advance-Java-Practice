7626498aa2f6e9bebc356a96a539eb56d032a487
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-22 23:47:11 +0530

    Multi Threading Example

--- Thread/MultiThreading.java
+++ Thread/MultiThreading.java
@@ -1,5 +1,36 @@
 package com.company;
 
-
 import java.lang.Thread;
 
+class MultithreadingDemo extends Thread
+{
+    public void run()
+    {
+        try
+        {
+            // Displaying the thread that is running
+            System.out.println ("Thread " +
+                    Thread.currentThread().getId() +
+                    " is running");
+
+        }
+        catch (Exception e)
+        {
+            // Throwing an exception
+            System.out.println ("Exception is caught");
+        }
+    }
+}
+
+class Multithread
+{
+    public static void main(String[] args)
+    {
+        int n = 8; // Number of threads
+        for (int i=0; i<n; i++)
+        {
+            MultithreadingDemo object = new MultithreadingDemo();
+            object.start();
+        }
+    }
+}
