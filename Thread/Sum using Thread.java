93f7901a2a300651aa4e272e9193ad71ea223655
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-23 22:48:35 +0530

    Update Sum using Thread.java

--- Thread/Sum using Thread.java
+++ Thread/Sum using Thread.java
@@ -3,3 +3,15 @@ package com.company;
 import java.util.Scanner;
 import java.lang.Thread;
 
+class sum extends Thread
+{
+    Scanner s = new Scanner(System.in);
+    public void run()
+    {   int a;
+        int b;
+        a=s.nextInt();
+        b=s.nextInt();
+       System.out.println(a+b);
+        System.out.println(Thread.currentThread());
+    }
+}
