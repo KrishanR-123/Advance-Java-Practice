0b6f8124909a7dc0acf05d49c27532987d533b57
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-27 21:45:06 +0530

    For understanding

--- Searching and sorting/Example 2.java
+++ Searching and sorting/Example 2.java
@@ -1,1 +1,27 @@
+package com.company;
 
+import java.util.Arrays;
+
+public class ArrayDemo {
+   public static void main(String[] args) {
+
+      // initializing unsorted array
+      Object arr[] = {10,2,22,69};
+
+      // sorting array
+      Arrays.sort(arr);
+
+      // let us print all the elements available
+      System.out.println("The sorted array is:");
+      for (Object number : arr) {
+         System.out.println("Number = " + number);
+      }
+
+      // entering the value to be searched
+      int searchVal = 22;
+
+      int retVal = Arrays.binarySearch(arr,searchVal);
+
+      System.out.println("The index of element 22 is : " + retVal);
+   }
+}
